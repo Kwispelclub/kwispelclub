ef2ec007b972eda90c0a28920268369bdc8d768c
Jansen Marc <jansen-marc@proton.me>
2026-04-30 02:04:47 +0200

    Fix getSession, academy systeem, demo cleanup

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -20,6 +20,7 @@ export default function Navbar() {
   const links = [
     { href: '/#shop', label: 'Shop' },
     { href: '/kapsalons', label: 'Kapsalons' },
+    { href: '/dierenarts', label: 'Dierenarts' },
     { href: '/2dehands', label: '2de Hands' },
     { href: '/puppy-training', label: 'Academy' },
     { href: '/blog', label: 'Blog' },
